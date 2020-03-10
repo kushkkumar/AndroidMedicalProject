9bb935a2e99cfa88abb46488043359f071364f45
kush <ankushkumar.n7@gmail.com>
2020-03-10 21:33:58 +0530

    edited and connected the fragments 10_3_20

--- frontEnd/MyApplication/app/src/main/java/com/example/myapplication/selectRegistration.java
+++ frontEnd/MyApplication/app/src/main/java/com/example/myapplication/selectRegistration.java
@@ -4,34 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class secondFragment extends Fragment {
+public class selectRegistration extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.second_fragment,container,false);
-
-        view.findViewById(R.id.doc_details).setOnClickListener(new View.OnClickListener() {
+        View view=inflater.inflate(R.layout.resgistration_first_fragment,container,false);
+        view.findViewById(R.id.permentReg).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(),"No doctor details are entered",Toast.LENGTH_SHORT).show();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame1,new permanentRegistration()).addToBackStack(null).commit();
             }
         });
-        view.findViewById(R.id.reg_btn).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.onetime).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame1,new selectRegistration()).addToBackStack(null).commit();
-
-
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame1,new onTimeActivity()).addToBackStack(null).commit();
             }
         });
-
         return view;
     }
 }

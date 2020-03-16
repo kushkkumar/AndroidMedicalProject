4a3ec4443c2e99642dc35c54046878332b396cd9
kush <ankushkumar.n7@gmail.com>
2020-03-16 15:51:38 +0530

    added

--- backEnd/server.js
+++ backEnd/server.js
@@ -12,9 +12,15 @@ const client = new Client({
   client.connect();
 
 
+var counttp=1;
+client.query("SELECT * FROM temppatient",(err,res)=>{
+    counttp=counttp+res.rowCount;
+   // console.log(counttp)
+})
+//console.log(counttp);
 
 
-
+//all the request from the android that are sent
 
 app.post('/temppatientRegistration',(request,response)=>{
     tempPatientRegistration(request,response);
@@ -25,6 +31,64 @@ app.post('/permanentRegistraion',(request,response)=>{
 })
 
 
+app.post('/bookSlotPermanent',(request,response)=>{
+    bookSlotPermanent(request,response);
+})
+
+app.post('/confirmPermanent',(request,response)=>{
+    confirmPermanent(request,response);
+})
+
+//the funtions that are needed to send back the information for the android
+
+
+function confirmPermanent(request,response){
+    client.query("SELECT * FROM permanentpatient",(err,res)=>{
+        var c="pt";
+        c=c+(res.rowCount.toString());
+
+
+
+        client.query("SELECT * FROM permanentpatient WHERE ppid='"+c+"';",(err,result)=>{
+            if(err) throw err
+    
+            if(result.rowCount==0){
+                response.status(410).send();
+            }
+            else{
+    
+            
+            response.status(210).send(JSON.stringify(result.rows[0]));
+           
+        }
+        
+        })
+        
+
+    })
+
+}
+
+function bookSlotPermanent(request,response){
+    const ppid=request.body.ppid;
+   
+
+    client.query("SELECT * FROM permanentpatient WHERE ppid='"+ppid+"';",(err,result)=>{
+        if(err) throw err
+
+        if(result.rowCount==0){
+            response.status(404).send();
+        }
+        else{
+
+        
+        response.status(202).send(JSON.stringify(result.rows[0]));
+       
+    }
+    
+    })
+}
+
 function permanentRegistraion(request,response){
     const name=request.body.name;
     const email=request.body.email;
@@ -33,19 +97,20 @@ function permanentRegistraion(request,response){
     const address=request.body.address;
     const gender=request.body.gender;
 
+    client.query("SELECT * FROM permanentpatient",(err,res)=>{
+        var d="pt";
+        d=d+((res.rowCount+1).toString());
+        console.log(d);
 
-
-client.query("INSERT INTO permanentpatient (ppid,name,bdate,gender,phoneno,email,address) VALUES ('pt01','"+name+"','"+bod+"','"+gender+"','"+phone+"','"+email+"','"+address+"');",(err,res)=>{
+client.query("INSERT INTO permanentpatient (ppid,name,bdate,gender,phoneno,email,address) VALUES ('"+d+"','"+name+"','"+bod+"','"+gender+"','"+phone+"','"+email+"','"+address+"');",(err,result)=>{
     if(err) response.status(401).send();
     response.status(201).send();
-    console.log("1 data inserted"+res);
+    console.log("1 data inserted"+result);
     
+})
 })
 
 }
-
-
-
 
 function tempPatientRegistration(request,response){
     const name=request.body.name;
@@ -55,23 +120,22 @@ function tempPatientRegistration(request,response){
     const address=request.body.address;
     const gender=request.body.gender;
 
+    client.query("SELECT * FROM temppatient",(err,res)=>{
+        var e="tp";
+        e=e+((res.rowCount+1).toString());
+        console.log(e);
 
 
-client.query("INSERT INTO temppatient (tpid,name,bdate,gender,phoneno,email,address) VALUES ('tp16','"+name+"','"+bod+"','"+gender+"','"+phone+"','"+email+"','"+address+"');",(err,res)=>{
+client.query("INSERT INTO temppatient (tpid,name,bdate,gender,phoneno,email,address) VALUES ('"+e+"','"+name+"','"+bod+"','"+gender+"','"+phone+"','"+email+"','"+address+"');",(err,result)=>{
     if(err) response.status(400).send();
     response.status(200).send();
-    console.log("1 data inserted"+res);
+    console.log("1 data inserted"+result);
     
 })
 
+    })
+
 }
-
-
-
-
-
-
-
 
 app.get('/',(req,response)=>{
 
@@ -80,29 +144,26 @@ app.get('/',(req,response)=>{
   
 })
 
-
-
 app.get('/table/temppatient',(request,response)=>{
     client.query('SELECT * FROM temppatient;', (err, res) => {
         if (err) throw err;
-       response.send(res);
+       response.send(res.rows);
         client.end();
       });
 })
 
-app.get('/table/permanentpatient',(request,response)=>{
+app.get('/table/permanentpatients',(request,response)=>{
     client.query('SELECT * FROM permanentpatient;', (err, res) => {
         if (err) throw err;
-       response.send(res);
+       response.send(res.rows);
         client.end();
       });
 })
-
 
 app.get('/table/doctordetails',(request,response)=>{
     client.query('SELECT * FROM doctordetails;', (err, res) => {
         if (err) throw err;
-       response.send(res);
+       response.send(res.rows);
         client.end();
       });
 })
@@ -110,33 +171,25 @@ app.get('/table/doctordetails',(request,response)=>{
 app.get('/table/client',(request,response)=>{
     client.query('SELECT * FROM client;', (err, res) => {
         if (err) throw err;
-       response.send(res);
+       response.send(res.rows);
         client.end();
       });
 })
-
 
 app.get('/table/permanentdiseasedetails',(request,response)=>{
     client.query('SELECT * FROM permanentdiseasedetails;', (err, res) => {
         if (err) throw err;
-       response.send(res);
+       response.send(res.rows);
         client.end();
       });
 })
-
-
 
 app.get('/table/permanentslot',(request,response)=>{
     client.query('SELECT * FROM permanentslot;', (err, res) => {
         if (err) throw err;
-       response.send(res);
+       response.send(res.rows);
         client.end();
       });
 })
 
-
-
-
-
-
-app.listen(process.env.PORT||9050,()=>console.log("its working "))
+app.listen(process.env.PORT||6005,()=>console.log("its working "))
